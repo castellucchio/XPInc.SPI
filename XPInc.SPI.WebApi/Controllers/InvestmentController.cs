9f21d00fc57c7f461fa3b6f308fa644b782d80b4
diegocastellucchio <castellucchiodiego@hotmail.com>
2024-07-16 07:31:11 -0300

    criado classes que representam operacoes de compra e venda de produtos por clientes e a api que receberá as solicitacoes http

---
+++ XPInc.SPI.WebApi/Controllers/InvestmentController.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XPInc.SPI.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvestmentController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IMediator _mediator;
+
+        public InvestmentController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpPost("buy")]
+        public async Task<IActionResult> Buy([FromBody] BuyRequest request)
+        {
+            //// Validar requisição
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            //// Consultar produto financeiro
+            //var product = await _financialProductService.GetByIdAsync(request.ProductId);
+            //if (product == null)
+            //{
+            //    return NotFound("Produto financeiro não encontrado");
+            //}
+
+            //// Validar saldo do cliente
+            //if (request.Quantity * product.CurrentPrice > GetCustomerBalance())
+            //{
+            //    return BadRequest("Saldo insuficiente");
+            //}
+
+            //// Realizar compra
+            //var transaction = await _transactionService.BuyAsync(request.ProductId, request.Quantity, product.CurrentPrice);
+
+            //// Atualizar saldo do cliente
+            //UpdateCustomerBalance(transaction.TotalAmount);
+
+            // Retornar resultado
+            return Ok();
+        }
+
+    }
+}
